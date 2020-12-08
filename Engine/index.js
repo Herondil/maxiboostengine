164eb86af9bc4b7da2ba16122c1b7a5422d9e257
Maxens Dubois <maxens.dubois@gmail.com>
2020-12-08 17:05:52 +0100

    Correction code animation

--- Engine/index.js
+++ Engine/index.js
@@ -2,15 +2,31 @@ let ctx,
 	width,
 	height;
 	
+//le sprite animé
+let img,
+	currentSx = 0;
+	
 //le dernier
 let lastClick = {
 	x : undefined,
 	y : undefined
 }
 
+let spriteLocation;
+
+let frames = 0;
+
 function StartGame(){
 	canvas_generator();
 	loadImgs();
+	
+	img = new Image();
+	img.src = spriteLocation;
+	
+	img.onload  = () => {
+		//démarer vraiment le jeu
+		window.requestAnimationFrame(gameLoop);
+	};
 }
 
 function loadImgs(){
@@ -58,18 +74,35 @@ function afficher_background(imageLocation){
 	};
 }
 
-function renderSprite(imageLocation, position){
-	//créér l'image, un élément image
-	let img = new Image();
-	img.src = imageLocation;
-
-	img.onload  = () => {
-		//afficher cette image dans le canvas
-		ctx.drawImage(img,0,0,96,64,position.x,position.y,96,64);
-	};
+function renderSprite(imageLocation, position, sx){
+	ctx.drawImage(img,sx,0,96,64,position.x,position.y,96,64);
 }
 
 function renderBackgroundColor(color){
+	
 	ctx.fillStyle = color;
 	ctx.fillRect(0, 0, 800, 600);
+}
+
+function gameLoop(){
+	
+	//dessin des éléments
+	ctx.clearRect(0, 0, 800,600)
+	
+
+	//dessiner le sprite de l'exercice
+	renderSprite(spriteLocation, {x : 50, y : 50}, currentSx);
+	
+	//passer à l'image suivante du sprite
+	// opération "modulo"
+	if( (frames % 6) == 0 ){
+		currentSx += 96;//aller à l'image clé suivante
+		if( currentSx == 672) currentSx = 0; //revenir au début de l'animation
+	}
+	
+	
+	
+	frames++;
+	//boucle du jeu
+	window.requestAnimationFrame(gameLoop);
 }
--- Games/Jeu2/script.js
+++ Games/Jeu2/script.js
@@ -1,9 +1,28 @@
 //import ....
 
+spriteLocation = "img/noBKG_KnightShield_strip.png";
+
 StartGame();
 
+
+
+
+/*
+
+
+renderSprite("img/noBKG_KnightShield_strip.png",{x : 50, y : 50},0);
 renderBackgroundColor("fuchsia");
+ctx.clearRect(0, 0, 800,600);
+renderSprite("img/noBKG_KnightShield_strip.png",{x : 80, y : 50},96*6);
+gameLoop();
 
-renderSprite("img/noBKG_KnightShield_strip.png",{x : 50, y : 50});
-
-renderSprite("img/noBKG_KnightShield_strip.png",{x : 500, y : 500});
+function gameLoop(){
+	//efface le précédent sprite, efface tout
+	//ctx.clearRect(0, 0, 800,600)
+	//afficher le sprite sur la bonne image
+	renderSprite("img/noBKG_KnightShield_strip.png",{x : 50, y : 50},0);
+	//console.log("gameLoop");
+	ctx.clearRect(0, 0, 800,600)
+	window.requestAnimationFrame(gameLoop);
+}
+*/
